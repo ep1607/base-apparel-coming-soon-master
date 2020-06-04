d671eddef0374e2b49c7155886f0c6450fd42246
ep1607 <eva.pocivavsek.janhar@gmail.com>
2020-06-04 18:59:51 +0200

    fixed button issue [it needs to have type submit]. --> changed also js file for validation to work properly

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
+var button = document.getElementById("button");
+var input = document.getElementById("email");
 
-function ValidateEmail(inputText) {
+button.addEventListener("click", validateEmail);
+
+function validateEmail() {
+
+    var email = input.value;
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(inputText.value.match(mailformat)) {
+
+    if(email.match(mailformat)) {
         document.getElementById("formError").innerHTML="";
-        document.getElementById("email_input").style.border="1px solid hsl(0, 36%, 70%)";
+        document.getElementById("email").style.border="1px solid hsl(0, 36%, 70%)";
         document.getElementById("error_icon").style.display="none";
         document.subscribeForm.femail.focus();
         return true;
     } else {
         document.getElementById("formError").innerHTML="Please provide valid email";
-        document.getElementById("email_input").style.border="2px solid hsl(0, 93%, 68%)";
+        document.getElementById("email").style.border="2px solid hsl(0, 93%, 68%)";
         document.getElementById("error_icon").style.display="block";
         document.subscribeForm.femail.focus();
         return false;
     }
+
  }
